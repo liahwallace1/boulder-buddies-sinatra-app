ddb5ada881ff5b808c85369c36a61556bec51bc9
Liah Wallace <liahwallace1@gmail.com>
2017-02-16 21:03:56 +0000

    updated state dropdown for new location form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,3 +1,22 @@
 class LocationsController < ApplicationController
 
+  get '/locations' do
+    @location = Location.all
+    erb :'/locations/locations'
+  end
+
+  get '/locations/new' do
+    erb :'locations/new'
+  end
+
+  post '/locations' do
+    @location = Location.create(params)
+    redirect to '/locations'
+  end
+
+  get '/locations/:slug' do
+      @location = Location.find_by_slug(params[:slug])
+      erb :'/locations/show'
+  end
+
 end
