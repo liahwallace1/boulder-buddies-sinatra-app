77b38f23f2b9f9369bcdb4568dafe548e2c85c94
Liah Wallace <liahwallace1@gmail.com>
2017-02-17 21:10:12 +0000

    creating new climbs form and controller action

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,26 +1,28 @@
 class ClimbsController < ApplicationController
 
-  get '/climbs' do
-    @climbs = Climb.all
-    erb :'/climbs/climbs'
-  end
-
   get '/climbs/new' do
     if session[:user_id]
+      @user = User.find(session[:user_id])
       erb :'climbs/new'
     else
       flash[:message] = "You have to be logged in to add a climb"
-      redirect to '/login'
+      redirect to '/locations'
     end
   end
 
-  # post '/climbs' do
-  #   @climb = Climb.create(params)
-  #   redirect to "/climbs/#{@climb.slug}"
-  # end
+  post '/climbs' do
+    @user = User.find(session[:user_id])
+    @climb = Climb.create(params[:climb])
+    @climb.user_id = @user.id
+    if !params["location"]["name"].empty?
+      @climb.location_id = Location.create(name: params["location"]["name"], city: params["location"]["city"], state: params["location"]["state"]).id
+    end
+    @climb.save
+    redirect to "/climbs/#{@climb.slug}"
+  end
 
   get '/climbs/:slug' do
-      @climbs = Climb.find_by_slug(params[:slug])
+      @climb = Climb.find_by_slug(params[:slug])
       erb :'/climbs/show'
   end
 
