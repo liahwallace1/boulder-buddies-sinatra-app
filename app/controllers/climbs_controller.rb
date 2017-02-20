0b3a0e9f57566dd45e2d23854c870e2e98bbf775
Liah Wallace <liahwallace1@gmail.com>
2017-02-20 16:17:20 +0000

    added edit climb ntrolller action

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -26,4 +26,26 @@ class ClimbsController < ApplicationController
       erb :'/climbs/show'
   end
 
+  get '/climbs/:slug/edit' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @climb = Climb.find_by_slug(params[:slug])
+      erb :'/climbs/edit'
+    else
+      flash[:message] = "You have to be logged in to edit a climb"
+      redirect to '/locations'
+    end
+  end
+
+  post '/climbs/:slug' do
+    @climb = Climb.find_by_slug(params[:slug])
+    @user = User.find(session[:user_id])
+    @climb.update(params[:climb])
+    if !params["location"]["name"].empty?
+      @climb.location_id = Location.create(name: params["location"]["name"], city: params["location"]["city"], state: params["location"]["state"]).id
+    end
+    @climb.save
+    redirect to "/climbs/#{@climb.slug}"
+  end
+
 end
